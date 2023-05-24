359bcfa6c4219d9b1728f508b488b72efa54d5c7
netkawai <netkawai@gmail.com>
2023-05-24 13:12:17 -0400

    Small changes for react learning

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,5 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from './logo.svg'
+
+const MyButton = () => {
+  const handleClick = () =>{
+    alert('You clicked me!')
+  }
+  return (
+    <button onClick={handleClick}>Click me</button>
+  );  
+}
+
+const ShoppingList = () => {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+  );
+
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'http://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+}
 
 const Hello = (props) => {
   return(
@@ -16,13 +56,25 @@ const App = () => {
 
   console.log('Hello from component')
   return (
+    <>
     <div>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>{user.name}</h1>
       <p>Hello World, it is {now.toString()}</p>
       <p>
         {a} plus {b} is {a+b}
       </p>
       <Hello name='Kawai' />
+      <MyButton />
+      <img
+      className="avatar"
+      src={user.imageUrl}
+      alt={'Photo of ' + user.name}
+      style={{width: user.imageSize, height: user.imageSize}}
+      />
+      <ShoppingList />
     </div>
+    </>
   )
 }
 
